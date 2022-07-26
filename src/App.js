ae12a70c0613c227c0cb8bc424799a143fe2641a
David Moina <davemolina777@gmail.com>
2022-07-26 08:51:34 -0500

    Create basic NavBar Component and Import into App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { About, Footer, Header, Skills, Testimonial, Work } from './containers'
+import { NavBar } from './components'
 
 const App = () => {
   return (
     <div className="app">
+      <NavBar />
       <Header />
       <About />
       <Work />
