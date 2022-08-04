eac19c143915b33bdb5ffbe7c34987d6b61a8f0a
David Moina <davemolina777@gmail.com>
2022-08-03 22:08:42 -0500

    Add scaling alternates

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -3,6 +3,17 @@ import { motion } from 'framer-motion';
 import { images } from '../../constants';
 import './Header.scss';
 
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut',
+    },
+  },
+};
+
 const Header = () => {
   return (
     <div className="app__header app__flex">
@@ -41,7 +52,17 @@ const Header = () => {
             />
           </motion.div>
       </motion.div>
-      Header
+      <motion.div
+      variants={scaleVariants}
+      whileInView={scaleVariants.whileInView}
+      className="app__header-circles"
+    >
+      {[images.flutter, images.redux, images.sass].map((circle, index) => (
+        <div className="circle-cmp app__flex" key={`circle-${index}`}>
+          <img src={circle} alt="profile_bg" />
+        </div>
+      ))}
+    </motion.div>
     </div>
   )
 }
