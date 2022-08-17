f9346b0f38471cb046f563867d111987cc7e7ef3
David Moina <davemolina777@gmail.com>
2022-08-16 22:03:23 -0500

    Create basic about section with abouts array

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.scss'
 const App = () => {
   return (
     <div className="app">
-      {/* <NavBar /> */}
+      <NavBar />
       <Header />
       <About />
       <Work />
--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+
 import './About.scss'
+
+const abouts = [
+  {title: "Web Development", description: 'I am a good web developer', imgURL: ''},
+  {title: "Front End", description: 'I am a good web developer', imgURL: ''},
+  {title: "Back End", description: 'I am a good web developer', imgURL: ''},
+  {title: "Adapting new technologies", description: 'I am a good web developer', imgURL: ''}
+]
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <h2 className="head-text">
+        I know that <span>Good Design</span><br /> means <span> good business</span>
+      </h2>
+    </>
   )
 }
 
