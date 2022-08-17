5ff7d14f41459c252d44ae859e911f2e40a950dc
David Moina <davemolina777@gmail.com>
2022-08-16 22:11:32 -0500

    Create loop for each object in array

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -3,7 +3,7 @@ import { motion } from 'framer-motion';
 
 import './About.scss'
 
-const abouts = [
+const aboutArray = [
   {title: "Web Development", description: 'I am a good web developer', imgURL: ''},
   {title: "Front End", description: 'I am a good web developer', imgURL: ''},
   {title: "Back End", description: 'I am a good web developer', imgURL: ''},
@@ -16,6 +16,21 @@ const About = () => {
       <h2 className="head-text">
         I know that <span>Good Design</span><br /> means <span> good business</span>
       </h2>
+      <div className="app__profiles">
+    {aboutArray.map((about, index) => (
+      <motion.div
+        whileInView={{ opacity: 1 }}
+        whileHover={{ scale: 1.1 }}
+        transition={{ duration: 0.5, type: 'tween'}}
+        className="app__profile-item"
+        key={about.title + index}
+        >
+          <img src={about.imgURL} alt={about.title}/>
+          <h2 className="bold-text" style={{ marginTop: 20 }}>{about.title}</h2>
+          <p className="p-text" style={{ marginTop: 10 }}>{about.description}</p>
+      </motion.div>
+    ))}
+      </div>
     </>
   )
 }
