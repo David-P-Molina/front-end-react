d6544c5794aef1560b9c57e9f17a344a080f24fb
David Moina <davemolina777@gmail.com>
2022-08-18 21:47:26 -0500

    Create basic for Higher Order Component

--- src/wrapper/AppWrap.js
+++ src/wrapper/AppWrap.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { NavigationDots, SocialMedia } from '../components'
 
-const AppWrap = () => {
+const AppWrap = (Component, idName, classNames) => function HOC() {
   return (
-    <div>AppWrap</div>
+    <div id={idName} className={`app__container ${classNames}`}>
+        <SocialMedia />
+        <div className="app__wrapper app__flex">
+            <Component />
+        </div>
+    </div>
   )
 }
 
